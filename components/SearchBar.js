80167695cf87c7d5965f06d5a0456a2ab8045b62
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-05-02 18:11:03 -0500

    Update MapSearchBar component styling
    The changes in this commit include updating the style of the MapSearchBar component by adding position, zIndex, and width to the container while changing the style of the input box. Also, the View component is imported and replaces the lowercase 'view', correcting a possible typo.

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,27 +1,31 @@
 import React, {useState} from 'react';
 import {SearchBar} from 'react-native-elements';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const MapSearchBar = () => {
     const [search, setSearch] = useState('');
 
     return (
-        <view>
+        <View style={styles.container}>
             <SearchBar
                 placeholder="Search for a location"
                 onChangeText={setSearch}
                 value={search}
                 lightTheme
                 round
-                style={styles.container}
+                containerStyle={{backgroundColor: 'transparent'}}
+                inputContainerStyle={{backgroundColor: 'white', paddingVertical: 0}}
             />
-        </view>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        // position: 'absolute', // Position the SearchBar absolutely
+        position: 'absolute',
+        top: 35,
+        width: '100%',
+        zIndex: 1,
     },
 });
 
